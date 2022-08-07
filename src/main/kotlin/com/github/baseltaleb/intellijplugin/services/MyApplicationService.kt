8e51466dd9522231fd6f39c3ac47ea2931a6fadc
GitHub Action <action@github.com>
2022-08-07 11:07:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/baseltaleb/intellijplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/baseltaleb/intellijplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.baseltaleb.intellijplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.baseltaleb.intellijplugin.MyBundle
 
 class MyApplicationService {
 
